e15f81f646510c7fb4bf03a0df2f378f8559edc9
iamLusani <lusani.22mphigalale@gmail.com>
2024-03-25 16:28:12 +0200

    added axios

--- src/app.js
+++ src/app.js
@@ -8,3 +8,15 @@ function searchCity(event) {
 
 let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", searchCity);
+
+function weatherApi(response) {
+  let temp = document.querySelector("#numberUnit");
+  let numberUnit = response.data.temperature.current;
+  let h1 = document.querySelector("h1");
+  console.log("response.data.temperature");
+}
+
+let city = "ermelo";
+let apiKey = "56ftc44o535ed4b849450cf23fa64b07";
+let apiUrl = ` https://api.shecodes.io/weather/v1/current?query= ${city}&key=${56ftc44o535ed4b849450cf23fa64b07} `;
+axios.get(apiUrl).then(weatherApi);
